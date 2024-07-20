72b45392cec9b05974af0845bed746b03ad66f46
Saurav kumar Arya <92774037+sauravkrarya@users.noreply.github.com>
2024-07-20 19:40:59 +0530

    Add files via upload

---
+++ lowerupper.java
@@ -0,0 +1,18 @@
+//Write a program to check whether a entered character is lowercase ( a to z ) or uppercase ( A to Z ).
+import java.util.*;
+class lowerupper
+{
+        public static void main (String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        char alp=sc.next().charAt(0);
+        int num=(int)alp;
+        if (num>=97 && num<=122)
+           {
+                System.out.print ((char)(num-32));
+           }
+        else{
+                System.out.print(alp);
+            }
+    }
+}
