4a5fa035357b2e185d9820240687287079a66516
Saurav kumar Arya <92774037+sauravkrarya@users.noreply.github.com>
2024-07-20 19:38:24 +0530

    Add files via upload

---
+++ gst.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class gst {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner (System.in);
+        int a=sc.nextInt();
+        float gst=(a*18)/100;
+        System.out.println(gst +a);
+    }
+    
+}
