a5245a3b511515427d1dd57b861f5748a52e51e7
Saurav kumar Arya <92774037+sauravkrarya@users.noreply.github.com>
2024-07-22 17:19:27 +0530

    Add files via upload

--- absolute.java
+++ absolute.java
@@ -1,17 +1,21 @@
-/* Write a program to print absolute value of a number entered by user. E.g.-
-INPUT: 1        OUTPUT: 1
-INPUT: -1        OUTPUT: 1 */
+import java.util.Scanner;
 
-import java.util.*;
 public class absolute {
- public static void main(String[] args) {
-    Scanner sc=new Scanner(System.in);
-    System.out.println("enter the number");
-    int num=sc.nextInt();
-   if (num<0)
-     System.out.println(num*(-1));
-   else
-     System.out.println(num);
 
+
+
+  public static float absolute(float abs){
+    return abs *-1;
   }
+
+  
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number :");
+        float abs =sc.nextFloat();
+        float a =absolute(abs);
+        System.out.println("Absolute value of the number is:" + a);
+        
+    }
+    
 }
