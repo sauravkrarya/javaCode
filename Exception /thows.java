2a7901d979c8964bf6b283fdd45b8859b4f94915
Saurav kumar Arya <92774037+sauravkrarya@users.noreply.github.com>
2024-08-12 01:59:20 +0530

    u

---
+++ Exception /thows.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.lang.*;
+public class throws {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        float a=sc.nextInt();
+
+}
